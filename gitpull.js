797d43b603f7edf2cc5c88a151765fad2d46524b
T.J. Eckman <jeek@jeek.net>
2022-02-17 15:54:12 -0500

    Update gitpull.js

--- gitpull.js
+++ gitpull.js
@@ -1,5 +1,5 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/gitpull2.js', 'jeek/gitpull2.js');
+	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/gitpull2.js', 'jeek/gitpull2.js');
 	ns.spawn('/jeek/gitpull2.js', 1);
 }
