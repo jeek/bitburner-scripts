4a5f30fd2a7bb152af7741f611511da4b794c8f6
T.J. Eckman <jeek@jeek.net>
2022-02-17 16:21:41 -0500

    wget needs leading slash for subdir

--- gitpull2.js
+++ gitpull2.js
@@ -1,6 +1,6 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/gitpull.js', 'jeek/gitpull.js');
+	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/gitpull.js', '/jeek/gitpull.js');
 	ns.spawn('/jeek/gitpull2.js', 1);
 	ns.toast('Scripts updated.');
 }
