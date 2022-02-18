a3583ae9486cc69bd37d00e3d7ada5f9cb6d0a15
T.J. Eckman <jeek@jeek.net>
2022-02-18 04:55:57 -0500

    Update checkprogs.js

--- checkprogs.js
+++ checkprogs.js
@@ -5,9 +5,9 @@ export async function main(ns) {
 	cost['BruteSSH.exe'] = 50;
 	cost['FTPCrack.exe'] = 100;
 	cost['relaySMTP.exe'] = 250;
-	cost['HTTPCrack.exe'] = 500;
+	cost['HTTPWorm.exe'] = 500;
 	cost['SQLInject.exe'] = 750;
-	var progs = ['BruteSSH.exe', 'FTPCrack.exe', 'relaySMTP.exe', 'HTTPCrack.exe', 'SQLInject.exe']
+	var progs = ['BruteSSH.exe', 'FTPCrack.exe', 'relaySMTP.exe', 'HTTPWorm.exe', 'SQLInject.exe']
 	for (var j in progs) {
 		var i = progs[j];
 		if (self.tor & !ns.fileExists(i)) {
