e5c63eb1fde437a15b1fb22d20479806bd8e6988
T.J. Eckman <jeek@jeek.net>
2022-02-18 01:13:05 -0500

    Update start.js

--- start.js
+++ start.js
@@ -32,6 +32,8 @@ async function bestserver(ns) {
 export async function main(ns) {
 	ns.disableLog("disableLog");
 	ns.disableLog("sleep");
+	ns.disableLog("scan");
+
 	var self = ns.getPlayer();
 	ns.tprint(self['hacking']);
 	while (self['hacking'] < 10) {
@@ -43,19 +45,90 @@ export async function main(ns) {
 		var self = ns.getPlayer();
 		await ns.sleep(100);
 	}
-	for (var prog in ['/jeek/purchasetor.js', '/jeek/checkprogs.js', '/jeek/upgradehomeram.js']) {
+	var bootstrap = ['/jeek/purchasetor.js', '/jeek/checkprogs.js', '/jeek/upgradehomeram.js'];
+	for (var progi in bootstrap) {
+		var prog = bootstrap[progi];
 		var pid = ns.run(prog, 1);
 		while (ns.isRunning(pid, 'home')) {
 			await ns.sleep(100);
 		}
 	}
+	var serverlist = ['home'];
 	var target = await bestserver(ns);
 	ns.toast('Best Server: ' + target);
-	var pid = ns.run('/jeek/simplehack.js', Math.floor((ns.getServerMaxRam('home') - ns.getServerUsedRam('home')) / ns.getScriptRam('/jeek/simplehack.js')), target);
-	if (pid > 0) {
-		while (ns.isRunning('/jeek/simplehack.js', 'home', target)) {
-			await ns.sleep(100);
+	var n00dles = ns.getServer(target);
+	while ((n00dles.minDifficulty + 5 < n00dles.hackDifficulty) | (n00dles.moneyAvailable < n00dles.moneyMax)) {
+		if (n00dles.minDifficulty + 5 < n00dles.hackDifficulty) {
+			ns.toast("Weaken " + target);
+			for (var i = 0; i < serverlist.length; i++) {
+				if (ns.hasRootAccess(serverlist[i])) {
+					if (serverlist[i] != 'home') {
+						await ns.scp('/jeek/weaken.js', serverlist[i]);
+					}
+					if (Math.floor((ns.getServerMaxRam(serverlist[i]) - ns.getServerUsedRam(serverlist[i])) / ns.getScriptRam('/jeek/weaken.js')) >= 1) {
+						ns.exec('/jeek/simplehack.js', serverlist[i], Math.floor((ns.getServerMaxRam(serverlist[i]) - ns.getServerUsedRam(serverlist[i])) / ns.getScriptRam('/jeek/weaken.js')), target);
+					}
+				}
+
+				var current = ns.scan(serverlist[i]);
+				for (var j = 0; j < current.length; j++) {
+					if (!serverlist.includes(current[j])) {
+						serverlist.push(current[j]);
+					}
+				}
+			}
+			while (ns.isRunning('/jeek/weaken.js', 'home', target)) {
+				await ns.sleep(100);
+			}
 		}
+		if (n00dles.moneyAvailable < n00dles.moneyMax) {
+			ns.toast("Grow on " + target);
+			for (var i = 0; i < serverlist.length; i++) {
+				if (ns.hasRootAccess(serverlist[i])) {
+					if (serverlist[i] != 'home') {
+						await ns.scp('/jeek/grow.js', serverlist[i]);
+					}
+					if (Math.floor((ns.getServerMaxRam(serverlist[i]) - ns.getServerUsedRam(serverlist[i])) / ns.getScriptRam('/jeek/grow.js')) >= 1) {
+						ns.exec('/jeek/grow.js', serverlist[i], Math.floor((ns.getServerMaxRam(serverlist[i]) - ns.getServerUsedRam(serverlist[i])) / ns.getScriptRam('/jeek/grow.js')), target);
+					}
+				}
+
+				var current = ns.scan(serverlist[i]);
+				for (var j = 0; j < current.length; j++) {
+					if (!serverlist.includes(current[j])) {
+						serverlist.push(current[j]);
+					}
+				}
+			}
+			while (ns.isRunning('/jeek/grow.js', 'home', target)) {
+				await ns.sleep(100);
+			}
+		}
+		n00dles = ns.getServer(target);
+	}
+	while (n00dles.moneyAvailable * 2 > n00dles.moneyMax) {
+		ns.toast("Hacking " + target);
+		for (var i = 0; i < serverlist.length; i++) {
+			if (ns.hasRootAccess(serverlist[i])) {
+				if (serverlist[i] != 'home') {
+					await ns.scp('/jeek/hack.js', serverlist[i]);
+				}
+				if (Math.floor((ns.getServerMaxRam(serverlist[i]) - ns.getServerUsedRam(serverlist[i])) / ns.getScriptRam('/jeek/hack.js')) >= 1) {
+					ns.exec('/jeek/hack.js', serverlist[i], Math.floor((ns.getServerMaxRam(serverlist[i]) - ns.getServerUsedRam(serverlist[i])) / ns.getScriptRam('/jeek/hack.js')), target);
+				}
+			}
+
+			var current = ns.scan(serverlist[i]);
+			for (var j = 0; j < current.length; j++) {
+				if (!serverlist.includes(current[j])) {
+					serverlist.push(current[j]);
+				}
+			}
+		}
+		n00dles = ns.getServer(target);
+	}
+	while (ns.isRunning('/jeek/hack.js', 'home', target)) {
+		await ns.sleep(100);
 	}
 	await ns.run('start.js', 1)
 }
