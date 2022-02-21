1f85bd4501b98935e493f63d2f430152584ea5d9
T.J. Eckman <jeek@jeek.net>
2022-02-21 14:54:02 -0500

    gitpull updates

--- gitpull.js
+++ gitpull.js
@@ -1,6 +1,10 @@
 /** @param {NS} ns **/
 export async function main(ns) {
+	var root = ns.read("/jtmp/root.txt");
+	if (root == "") {
+		root = "https://raw.githubusercontent.com/jeek/bitburner/main/";
+	}
 	ns.tprint("Downloading jeekOS... getting list of files...");
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/gitpull2.js', '/jeek/gitpull2.js');
+	await ns.wget(root + 'gitpull2.js', '/jeek/gitpull2.js');
 	ns.spawn('/jeek/gitpull2.js', 1);
 }
--- gitpull2.js
+++ gitpull2.js
@@ -1,26 +1,30 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 	ns.tprint("Downloading jeekOS... downloading files...");
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/gitpull.js', '/jeek/gitpull.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/hackit.js', '/jeek/hackit.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/start.js', '/jeek/start.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/purchasetor.js', '/jeek/purchasetor.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/simplehack.js', '/jeek/simplehack.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/upgradehomeram.js', '/jeek/upgradehomeram.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/checkprogs.js', '/jeek/checkprogs.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/grow.js', '/jeek/grow.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/hack.js', '/jeek/hack.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/weaken.js', '/jeek/weaken.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/start2.js', '/jeek/start2.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/purchaseservers.js', '/jeek/purchaseservers.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/pop_all.js', '/jeek/pop_all.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/batch.js', '/jeek/batch.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/commitcrime.js', '/jeek/commitcrime.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/installbackdoors.js', '/jeek/installbackdoors.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/checkfirst.js', '/jeek/checkfirst.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/checksource.js', '/jeek/checksource.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/POST.js', '/jeek/POST.js');
-	await ns.wget('https://raw.githubusercontent.com/jeek/bitburner/main/metrics.js', '/jeek/metrics.js');
+	var root = ns.read("/jtmp/root.txt");
+	if (root == "") {
+		root = "https://raw.githubusercontent.com/jeek/bitburner/main/";
+	}
+	await ns.wget(root + 'gitpull.js', '/jeek/gitpull.js');
+	await ns.wget(root + 'hackit.js', '/jeek/hackit.js');
+	await ns.wget(root + 'start.js', '/jeek/start.js');
+	await ns.wget(root + 'purchasetor.js', '/jeek/purchasetor.js');
+	await ns.wget(root + 'simplehack.js', '/jeek/simplehack.js');
+	await ns.wget(root + 'upgradehomeram.js', '/jeek/upgradehomeram.js');
+	await ns.wget(root + 'checkprogs.js', '/jeek/checkprogs.js');
+	await ns.wget(root + 'grow.js', '/jeek/grow.js');
+	await ns.wget(root + 'hack.js', '/jeek/hack.js');
+	await ns.wget(root + 'weaken.js', '/jeek/weaken.js');
+	await ns.wget(root + 'start2.js', '/jeek/start2.js');
+	await ns.wget(root + 'purchaseservers.js', '/jeek/purchaseservers.js');
+	await ns.wget(root + 'pop_all.js', '/jeek/pop_all.js');
+	await ns.wget(root + 'batch.js', '/jeek/batch.js');
+	await ns.wget(root + 'commitcrime.js', '/jeek/commitcrime.js');
+	await ns.wget(root + 'installbackdoors.js', '/jeek/installbackdoors.js');
+	await ns.wget(root + 'checkfirst.js', '/jeek/checkfirst.js');
+	await ns.wget(root + 'checksource.js', '/jeek/checksource.js');
+	await ns.wget(root + 'POST.js', '/jeek/POST.js');
+	await ns.wget(root + 'metrics.js', '/jeek/metrics.js');
 	ns.toast('Scripts updated.');
 	ns.spawn("/jeek/checkfirst.js");
 }
